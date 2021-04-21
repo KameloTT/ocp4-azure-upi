6948bf41eaa83b9667f121468d9b3836a6926b1e
Alexandr Vishniakov <avishnia@redhat.com>
2021-04-20 20:09:28 -0400

    Clean up unnessesary resources

--- dns/variables.tf
+++ dns/variables.tf
@@ -13,31 +13,12 @@ variable "base_domain_resource_group_name" {
   type        = "string"
 }
 
-variable "external_lb_fqdn" {
-  description = "External API's LB fqdn"
-  type        = "string"
-}
-
-variable "internal_lb_ipaddress" {
-  description = "External API's LB Ip address"
-  type        = "string"
-}
 
 variable "private_dns_zone_name" {
   description = "private DNS zone name that should be used for records"
   type        = "string"
 }
 
-variable "etcd_count" {
-  description = "The number of etcd members."
-  type        = "string"
-}
-
-variable "etcd_ip_addresses" {
-  description = "List of string IPs for machines running etcd members."
-  type        = list(string)
-  default     = []
-}
 
 variable "resource_group_name" {
   type        = "string"
--- main.tf
+++ main.tf
@@ -12,24 +12,6 @@ provider "azurerm" {
   tenant_id       = "${var.azure_tenant_id}"
 }
 
-module "bootstrap" {
-  source                  = "./bootstrap"
-  resource_group_name     = "${azurerm_resource_group.main.name}"
-  region                  = "${var.azure_region}"
-  vm_size                 = "${var.azure_bootstrap_vm_type}"
-  vm_image                = "${var.azure_image_id}"
-  identity                = "${azurerm_user_assigned_identity.main.id}"
-  cluster_id              = "${var.cluster_id}"
-  ignition                = "${data.ignition_config.bootstrap_redirect.rendered}"
-  subnet_id               = "${module.vnet.public_subnet_id}"
-  elb_backend_pool_id     = "${module.vnet.public_lb_backend_pool_id}"
-  ilb_backend_pool_id     = "${module.vnet.internal_lb_backend_pool_id}"
-  boot_diag_blob_endpoint = "${azurerm_storage_account.bootdiag.primary_blob_endpoint}"
-  nsg_name                = "${module.vnet.master_nsg_name}"
-
-  # This is to create explicit dependency on private zone to exist before VMs are created in the vnet. https://github.com/MicrosoftDocs/azure-docs/issues/13728
-  private_dns_zone_id = "${azurerm_dns_zone.private.id}"
-}
 
 module "vnet" {
   source              = "./vnet"
@@ -40,28 +22,6 @@ module "vnet" {
   cluster_id          = "${var.cluster_id}"
   region              = "${var.azure_region}"
   dns_label           = "${var.cluster_id}"
-
-  # This is to create explicit dependency on private zone to exist before VMs are created in the vnet. https://github.com/MicrosoftDocs/azure-docs/issues/13728
-  private_dns_zone_id = "${azurerm_dns_zone.private.id}"
-}
-
-module "master" {
-  source                  = "./master"
-  resource_group_name     = "${azurerm_resource_group.main.name}"
-  cluster_id              = "${var.cluster_id}"
-  region                  = "${var.azure_region}"
-  vm_size                 = "${var.azure_master_vm_type}"
-  vm_image                = "${var.azure_image_id}"
-  identity                = "${azurerm_user_assigned_identity.main.id}"
-  ignition                = "${data.ignition_config.master_redirect.rendered}"
-  external_lb_id          = "${module.vnet.public_lb_id}"
-  elb_backend_pool_id     = "${module.vnet.public_lb_backend_pool_id}"
-  ilb_backend_pool_id     = "${module.vnet.internal_lb_backend_pool_id}"
-  subnet_id               = "${module.vnet.public_subnet_id}"
-  master_subnet_cidr      = "${local.master_subnet_cidr}"
-  instance_count          = "${var.master_count}"
-  boot_diag_blob_endpoint = "${azurerm_storage_account.bootdiag.primary_blob_endpoint}"
-  os_volume_size          = "${var.azure_master_root_volume_size}"
 
   # This is to create explicit dependency on private zone to exist before VMs are created in the vnet. https://github.com/MicrosoftDocs/azure-docs/issues/13728
   private_dns_zone_id = "${azurerm_dns_zone.private.id}"
@@ -120,98 +80,4 @@ resource "azurerm_virtual_network" "cluster_vnet" {
   resource_group_name = "${azurerm_resource_group.main.name}"
   location            = "${var.azure_region}"
   address_space       = ["${var.machine_cidr}"]
-}
-
-resource "azurerm_storage_account" "ignition" {
-  name                     = "ignition${local.cluster_nr}"
-  resource_group_name      = "${azurerm_resource_group.main.name}"
-  location                 = "${var.azure_region}"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-data "azurerm_storage_account_sas" "ignition" {
-  connection_string = "${azurerm_storage_account.ignition.primary_connection_string}"
-  https_only        = true
-
-  resource_types {
-    service   = false
-    container = false
-    object    = true
-  }
-
-  services {
-    blob  = true
-    queue = false
-    table = false
-    file  = false
-  }
-
-  start  = timestamp()
-
-  expiry = timeadd(timestamp(), "24h")
-
-  permissions {
-    read    = true
-    list    = true
-    create  = false
-    add     = false
-    delete  = false
-    process = false
-    write   = false
-    update  = false
-  }
-}
-
-resource "azurerm_storage_container" "ignition" {
-  resource_group_name   = "${azurerm_resource_group.main.name}"
-  name                  = "ignition"
-  storage_account_name  = "${azurerm_storage_account.ignition.name}"
-  container_access_type = "private"
-}
-
-resource "azurerm_storage_blob" "ignition-bootstrap" {
-  name                   = "bootstrap.ign"
-  source                 = "ignition-files/bootstrap.ign"
-  resource_group_name    = "${azurerm_resource_group.main.name}"
-  storage_account_name   = "${azurerm_storage_account.ignition.name}"
-  storage_container_name = "${azurerm_storage_container.ignition.name}"
-  type                   = "block"
-}
-
-resource "azurerm_storage_blob" "ignition-master" {
-  name                   = "master.ign"
-  source                 = "ignition-files/master.ign"
-  resource_group_name    = "${azurerm_resource_group.main.name}"
-  storage_account_name   = "${azurerm_storage_account.ignition.name}"
-  storage_container_name = "${azurerm_storage_container.ignition.name}"
-  type                   = "block"
-}
-
-data "ignition_config" "master_redirect" {
-  replace {
-    source = "${azurerm_storage_blob.ignition-master.url}${data.azurerm_storage_account_sas.ignition.sas}"
-  }
-}
-
-data "ignition_config" "bootstrap_redirect" {
-  replace {
-    source = "${azurerm_storage_blob.ignition-bootstrap.url}${data.azurerm_storage_account_sas.ignition.sas}"
-  }
-}
-
-resource "azurerm_storage_blob" "ignition-worker" {
-  name                   = "worker.ign"
-  source                 = "ignition-files/worker.ign"
-  resource_group_name    = "${azurerm_resource_group.main.name}"
-  storage_account_name   = "${azurerm_storage_account.ignition.name}"
-  storage_container_name = "${azurerm_storage_container.ignition.name}"
-  type                   = "block"
-}
-
-
-data "ignition_config" "worker_redirect" {
-  replace {
-    source = "${azurerm_storage_blob.ignition-worker.url}${data.azurerm_storage_account_sas.ignition.sas}"
-  }
 }
